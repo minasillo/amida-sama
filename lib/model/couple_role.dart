b5ba7b959c298195fda669cf8ebb7211c5914793
minasillo <163367347+minasillo@users.noreply.github.com>
2026-09-17 22:56:34 +0900

    Update CoupleRole enum with new role names

--- lib/model/couple_role.dart
+++ lib/model/couple_role.dart
@@ -1,8 +1,8 @@
 import 'package:amidakuji_app/gen/assets.gen.dart';
 
 enum CoupleRole {
-  groom(roleName: '新郎'),
-  bride(roleName: '新婦'),
+  groom(roleName: '阿弥陀'),
+  bride(roleName: '如来'),
   ;
 
   const CoupleRole({required this.roleName});
